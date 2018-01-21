a0fd2d5b4489432c93ba9fb1b0c284d19a5fdbd2
Luis Ybarra <lrybarra@gmail.com>
2018-01-21 17:58:37 -0600

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'clients/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
