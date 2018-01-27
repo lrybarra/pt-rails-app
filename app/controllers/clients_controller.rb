4fac4f91bba9698a69f2e6f7a906376a8ee1ae72
Luis Ybarra <lrybarra@gmail.com>
2018-01-27 15:59:52 -0600

    yo

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,25 @@
 class ClientsController < ApplicationController
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    @client.save
+    redirect_to @client
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
   def index
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :weight, :start)
   end
 end
